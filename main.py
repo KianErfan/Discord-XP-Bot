0282a133e19f9eabb9fd4ff78618fa7cace554d9
KianErfan Gaming <66801062+KianErfan@users.noreply.github.com>
2021-01-03 20:32:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ cogs = [exp]
 client = commands.Bot(command_prefix="?", intents=discord.Intents.all())
 client.remove_command("help")
 
+def on_ready()
+await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Your Xp"))
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
