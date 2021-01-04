cb5cb02cf5a568a0fd812d9ccd5751ffe75b92a6
KianErfan Gaming <66801062+KianErfan@users.noreply.github.com>
2021-01-03 20:01:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from discord.ext import commands
 from pymongo import MongoClient
 
 import exp
+import token
 
 cogs = [exp]
 
 client = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+client.remove_command("help")
 
 
 for i in range(len(cogs)):
